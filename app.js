f385ae472b10c790d45556ec16a8c2e4f3a25d50
Piyush <piyushpatil98@protonmail.com>
2021-03-24 12:34:41 +0530

    Heroku initilise 1

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const app=express();
 const path=require('path');
 let PORT=process.env.PORT||3000;
 const router=require('./router/router.js');
-// Static Middleware
+// Template Engine
 app.set("views", path.join(__dirname, "views"));
 app.set('view engine','pug');
+// Static Middleware
 app.use(express.static('public'));
 // Router
 app.use('/',router);
-
 // Server listening
 app.listen(PORT,()=>{
 	console.log(`listening PORT http://localhost:${PORT} ...`);
